9cf34cd356d40628fe856c3bf6c6c2a3fcc493fb
Sébastien <svidal1@free.fr>
2023-08-29 10:32:46 +0200

    improve tests

--- test/0_compilation_typescript.js
+++ test/0_compilation_typescript.js
@@ -17,7 +17,8 @@
 
 describe("compilation typescript", () => {
 
-	const compilationTarget = join(__dirname, "typescript", "compilation.js");
+	const compilationSource = join(__dirname, "typescript", "compilation.cts");
+	const compilationTarget = join(__dirname, "typescript", "compilation.cjs");
 
 	before(() => {
 
@@ -47,15 +48,42 @@ describe("compilation typescript", () => {
 
 	});
 
-	it("should compile typescript file", (done) => {
+	it("should compile typescript file", () => {
 
-		exec("tsc " + join(__dirname, "typescript", "compilation.ts"), {
+		return new Promise((resolve, reject) => {
+
+			const args = [
+				"npx tsc",
+				compilationSource,
+				"--target es6",
+				"--module commonjs"
+			];
+
+			exec(args.join(" "), {
+				"cwd": join(__dirname, ".."),
+				"windowsHide": true
+			}, (err) => {
+				return err ? reject(err) : resolve();
+			});
+
+		});
+
+	}).timeout(MAX_TIMEOUT);
+
+	it("should exec compiled typescript file", (done) => {
+
+		const args = [
+			"node",
+			compilationTarget
+		];
+
+		exec(args.join(" "), {
 			"cwd": join(__dirname, ".."),
 			"windowsHide": true
 		}, (err) => {
 			return err ? done(err) : done();
 		});
 
-	}).timeout(MAX_TIMEOUT);
+	});
 
 });
