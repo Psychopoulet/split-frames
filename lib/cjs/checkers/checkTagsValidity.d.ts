a5c62f5e702d296c176186b555bc1759d8e74ddc
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-04-12 02:48:55 +0200

    remove builded back

--- lib/cjs/checkers/checkTagsValidity.d.ts
+++
@@ -1,2 +0,0 @@
-import type { tTag } from "../SplitFrames.js";
-export default function checkTagsValidity(tags: tTag): boolean;
--- lib/cjs/checkers/initData.d.ts
+++
@@ -1,2 +0,0 @@
-import type { iOptions, iConf } from "../SplitFrames.js";
-export default function initData(options?: iOptions): iConf;
